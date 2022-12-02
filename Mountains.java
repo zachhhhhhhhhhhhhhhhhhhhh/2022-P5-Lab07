d8b422d0ea996ab70962bdb58dc4f1787a53ec0a
zach <zachary.schwerdtfeger@vtsd.com>
2022-12-02 13:53:14 -0500

    the thing

---
+++ Mountains.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.applet.*;
+public class Mountains extends Applet{
+
+    public void paint(Graphics g){
+
+
+        //background
+        g.drawground(g);
+        g.drawSky(g);
+
+
+        //mountains
+        g.drawPeaks(g);
+        g.drawsnow(g);
+        g.drawFlag(g);
+        g.drawSun(g);
+
+
+        //fields
+        g.drawTree(g);
+        g.drawBushes(g);
+        g.drawHouse(g);
+        g.drawLake(g);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
